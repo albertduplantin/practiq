50e61a4756db6df230677c8ba800654b71e4b3b2
albertduplantin <topinambour124@gmail.com>
2025-10-06 01:01:37 +0200

    Fix ESLint apostrophes in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,8 +6,8 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", padding: "2rem" }}>
       <h1>404 - Page Non Trouvée</h1>
-      <p>Désolé, la page que vous cherchez n'existe pas.</p>
-      <Link href="/">Retour à l'accueil</Link>
+      <p>Désolé, la page que vous cherchez n&apos;existe pas.</p>
+      <Link href="/">Retour à l&apos;accueil</Link>
     </div>
   );
 }
